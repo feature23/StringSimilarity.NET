71bfadb1a5efade0cc8859f9fa37d195dd264e58
James Blair <james.blair@outlook.com>
2016-08-22 16:28:33 -0400

    Fixed failing tests

---
+++ src/StringSimilarity.Tests/CosineTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringSimilarity.Tests
+{
+    [TestClass]
+    public class CosineTest
+    {
+        [TestMethod]
+        public void TestSimilarity()
+        {
+            var instance = new Cosine();
+
+            double result = instance.Similarity("ABC", "ABCE");
+
+            Assert.AreEqual(0.71, result, 0.01);
+        }
+
+        [TestMethod]
+        public void TestSmallString()
+        {
+            var instance = new Cosine(3);
+
+            double result = instance.Similarity("AB", "ABCE");
+
+            Assert.AreEqual(0.0, result, 0.00001);
+        }
+
+        [TestMethod]
+        public async Task TestLargeString()
+        {
+            var instance = new Cosine();
+
+            // Read text from two files
+            var string1 = await ReadResourceFileAsync("71816-2.txt");
+            var string2 = await ReadResourceFileAsync("11328-1.txt");
+
+            double result = instance.Similarity(string1, string2);
+
+            Assert.AreEqual(0.8115, result, 0.001);
+        }
+
+        private static async Task<string> ReadResourceFileAsync(string file)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = $"{typeof(CosineTest).Namespace}.{file}";
+
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
+            using (var reader = new StreamReader(stream))
+            {
+                return await reader.ReadToEndAsync();
+            }
+        }
+    }
+}
---
+++ src/StringSimilarity/Utils/SparseBooleanVector.cs
@@ -0,0 +1,133 @@
+﻿/*
+ * The MIT License
+ *
+ * Copyright 2016 feature[23]
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringSimilarity.Utils
+{
+    public class SparseBooleanVector
+    {
+        public int[] Keys { get; protected set; }
+        public int Size => Keys.Length;
+
+        public SparseBooleanVector() : this(20) { }
+
+        public SparseBooleanVector(int size)
+        {
+            Keys = new int[size];
+        }
+
+        public SparseBooleanVector(IDictionary<int, int> dictionary) 
+            : this(dictionary.Count)
+        {
+            int size = 0;
+
+            foreach (var key in dictionary.Keys.OrderBy(k => k))
+            {
+                Keys[size] = key;
+                size++;
+            }
+        }
+
+        public SparseBooleanVector(bool[] array)
+        {
+
+            int size = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i])
+                {
+                    size++;
+                }
+            }
+
+            Keys = new int[size];
+            int j = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i])
+                {
+                    Keys[j] = i;
+                    j++;
+                }
+            }
+        }
+
+        public double Jaccard(SparseBooleanVector other)
+        {
+            int intersection = this.Intersection(other);
+            return (double)intersection / (this.Size + other.Size - intersection);
+        }
+
+        public int Union(SparseBooleanVector other)
+        {
+            return this.Size + other.Size - this.Intersection(other);
+        }
+
+        public int Intersection(SparseBooleanVector other)
+        {
+            int agg = 0;
+            int i = 0;
+            int j = 0;
+
+            while (i < Keys.Length && j < other.Keys.Length)
+            {
+                int k1 = Keys[i];
+                int k2 = other.Keys[j];
+
+                if (k1 == k2)
+                {
+                    agg++;
+                    i++;
+                    j++;
+                }
+                else if (k1 < k2)
+                {
+                    i++;
+
+                }
+                else
+                {
+                    j++;
+                }
+            }
+
+            return agg;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < Size; i++)
+            {
+                sb.Append($"{Keys[i]}:{Keys[i]} ");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
