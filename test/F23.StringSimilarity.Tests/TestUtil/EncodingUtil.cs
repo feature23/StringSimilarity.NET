1ce235de838d179fd03ebdba54cf4ac6b4a3c306
Paul Irwin <paulirwin@gmail.com>
2024-08-09 14:55:46 -0600

    Add multi-targeting to unit test project with net481 support

---
+++ test/F23.StringSimilarity.Tests/TestUtil/EncodingUtil.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+namespace F23.StringSimilarity.Tests.TestUtil;
+
+internal class EncodingUtil
+{
+    public static Encoding Latin1 =>
+#if NET5_0_OR_GREATER
+        Encoding.Latin1;
+#else
+        Encoding.GetEncoding("ISO-8859-1");
+#endif
+}
